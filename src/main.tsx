712573ece18bf7656aa0d01bea312d78b3fd0ff4
dungtahvn-max <dungta.hvn@gmail.com>
2026-03-16 15:08:03 +0700

    ver4

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,13 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// Fix for "Cannot set property fetch of #<Window> which has only a getter"
+// This can happen if a library tries to polyfill fetch in an environment where it's read-only.
+if (typeof window !== 'undefined') {
+  // @ts-ignore
+  window.global = window;
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
